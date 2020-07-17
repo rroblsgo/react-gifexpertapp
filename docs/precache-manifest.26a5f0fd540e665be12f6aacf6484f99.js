566b399296c6927ff6b390f3b3da6077706d3732
Ricardo Robles <rroblesgo@gmail.com>
2020-07-17 22:06:42 +0200

    docs agregado

---
+++ docs/precache-manifest.26a5f0fd540e665be12f6aacf6484f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94aba58f22e104808f8016bc903915e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6015af92d40658d9584",
+    "url": "/static/css/main.607c770d.chunk.css"
+  },
+  {
+    "revision": "7e4dc33d002342e8dafa",
+    "url": "/static/js/2.244b7771.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.244b7771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6015af92d40658d9584",
+    "url": "/static/js/main.d0a7b7b6.chunk.js"
+  },
+  {
+    "revision": "734d1024b223b571db93",
+    "url": "/static/js/runtime-main.deb3e66d.js"
+  }
+]);
