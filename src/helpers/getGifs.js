4e5cf6eaade6faabc7c3a48f86886f49c7fa3705
Ricardo Robles <rroblesgo@gmail.com>
2020-07-17 22:04:56 +0200

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifs = async category => {
+  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=U9dsPS74aMi2GcL7AiVzIEBkoSWZVrXV`
+  const resp = await fetch(url)
+  const { data } = await resp.json()
+
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    }
+  })
+  return gifs
+}
